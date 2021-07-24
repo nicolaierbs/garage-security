0b0c62eeb574a076744310d381d6a5a0497dc691
Nicolai Erbs <nico@erbs.eu>
2021-07-24 12:41:55 +0200

    Initial project setup

---
+++ camera_capturer.py
@@ -0,0 +1,80 @@
+import cv2
+import logging
+import time
+from datetime import datetime, timedelta
+from pathlib import Path
+import configparser
+import sys
+
+params = configparser.ConfigParser()
+params.read('parameters.ini')
+
+image_path = params.get('STORAGE', 'path')
+
+
+def configure_logger():
+    logger = logging.getLogger('standard_logger')
+    logger.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('debug.log')
+    fh.setLevel(logging.DEBUG)
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    # add the handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    return logger
+
+
+def hierarchical_file(date, suffix):
+    path = image_path + date.strftime("%Y/%m/%d/%H/")
+    Path(path).mkdir(parents=True, exist_ok=True)
+    return path + str(int(date.timestamp())) + '.' + suffix
+
+
+def lights(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    print(hsv[:, :, 2].mean())
+
+    return True
+
+
+def take_images():
+    # initialize the camera
+    cam = cv2.VideoCapture(1)   # 0 -> index of camera
+    # Obtain frame size information using get() method
+    frame_width = int(cam.get(3))
+    frame_height = int(cam.get(4))
+    frame_size = (frame_width, frame_height)
+    fps = 20
+
+    log.info('Initialized camera capturing')
+    captured = True
+    while captured:
+        captured, img = cam.read()
+        time.sleep(1)
+        if lights(img):
+            log.info('Detected light')
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            out = cv2.VideoWriter(hierarchical_file(datetime.now(), 'mp4'),
+                                  fourcc, fps, frame_size)
+            end_time = datetime.now() + timedelta(seconds=20)
+            while datetime.now() < end_time:
+                ret, frame = cam.read()
+                out.write(frame)
+            out.release()
+
+        elif not captured:
+            log.error('No image captured')
+            sys.exit(1)
+
+
+log = configure_logger()
+log.info('Started camera_capturer')
+take_images()
+log.info('End camera-capturer')
---
+++ disk_manager.py
@@ -0,0 +1,51 @@
+import shutil
+import subprocess
+import os
+import configparser
+from time import time
+from datetime import datetime
+
+config_section = 'STORAGE'
+params = configparser.ConfigParser()
+params.read('parameters.ini')
+
+image_path = params.get(config_section, 'path')
+delete_delay = params.getint(config_section, 'delay_storage')
+
+
+def get_disk_space():
+    total, used, free = shutil.disk_usage(image_path)
+    return str(free // (2**30)) + 'GB von ' + str(total // (2**30)) + 'GB'
+
+
+def path_size(path):
+    """disk usage in human readable format (e.g. '2,1GB')"""
+    return subprocess.check_output(['du','-sh', path]).split()[0].decode('utf-8')
+
+
+def video_count(path):
+    count = 0
+    new_path, directories, files = next(os.walk(path))
+    count += len(files)
+    for directory in directories:
+        count += video_count(path + '/' + directory)
+    return count
+
+
+def delete_old_files(path, delay):
+    new_path, directories, files = next(os.walk(path))
+    for file in files:
+        age = time() - float(os.path.getmtime(path + '/' + file))
+        age = age/3600/24
+        if file.endswith('.jpg') and age > delay:
+            os.remove(path + '/' + file)
+    for directory in directories:
+        delete_old_files(path + '/' + directory, delay)
+
+
+def main():
+    delete_old_files(image_path, delete_delay)
+
+
+if __name__ == "__main__":
+    main()
